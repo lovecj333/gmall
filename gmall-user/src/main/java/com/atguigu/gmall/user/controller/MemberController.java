6d4245b287e7160cec2bc1fe1e60778cf6f85dde
chenjian <chenjian_336@126.com>
2019-09-11 10:33:32 +0800

    study commit

--- gmall-user/src/main/java/com/atguigu/gmall/user/controller/MemberController.java
+++ gmall-user/src/main/java/com/atguigu/gmall/user/controller/MemberController.java
@@ -1,12 +1,13 @@
 package com.atguigu.gmall.user.controller;
 
-import com.atguigu.gmall.user.bean.UmsMember;
-import com.atguigu.gmall.user.bean.UmsMemberReceiveAddress;
-import com.atguigu.gmall.user.service.MemberService;
+import com.atguigu.gmall.bean.UmsMember;
+import com.atguigu.gmall.bean.UmsMemberReceiveAddress;
+import com.atguigu.gmall.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.List;
 
 @Controller
--- gmall-user/src/main/java/com/atguigu/gmall/user/service/impl/MemberServiceImpl.java
+++ gmall-user/src/main/java/com/atguigu/gmall/user/service/impl/MemberServiceImpl.java
@@ -1,16 +1,16 @@
 package com.atguigu.gmall.user.service.impl;
 
-import com.atguigu.gmall.user.bean.UmsMember;
-import com.atguigu.gmall.user.bean.UmsMemberReceiveAddress;
+import com.atguigu.gmall.bean.UmsMember;
+import com.atguigu.gmall.bean.UmsMemberReceiveAddress;
+import com.atguigu.gmall.service.MemberService;
 import com.atguigu.gmall.user.mapper.MemberMapper;
 import com.atguigu.gmall.user.mapper.MemberReceiveAddressMapper;
-import com.atguigu.gmall.user.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class MemberServiceImpl implements MemberService{
+public class MemberServiceImpl implements MemberService {
 
     @Autowired
     private MemberMapper memberMapper;
